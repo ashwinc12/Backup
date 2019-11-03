d8ba30531f8adde613953a612e9e9fe741289b91
Ashwin Chitoor <ashketchup55@gmail.com>
2019-11-03 10:08:15 -0500

    added upload to google storage

--- pi_side/auto_run_camera.py
+++ pi_side/auto_run_camera.py
@@ -3,27 +3,28 @@ import time
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(-1)
+time.sleep(30)
 # Define the codec and create VideoWriter object
-if (cap.isOpened()== False):  
-  print("Error opening video  file")
-  
+if (cap.isOpened() == False):
+    print("Error opening video  file")
+
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
 start_time = time.time()
 
-while(cap.isOpened()):
+while (cap.isOpened()):
     ret, frame = cap.read()
     current_time = time.time()
-    if ret==True:
+    if ret == True:
         # frame = cv2.flip(frame,0)
 
         # write the flipped frame
         out.write(frame)
 
-        cv2.imshow('frame',frame)
-        if (current_time - start_time) > 10:
-            break
+        cv2.imshow('frame', frame)
+    if (current_time - start_time) > 2:
+        break
 
 # Release everything if job is finished
 cap.release()
--- pi_side/check_end.py
+++ pi_side/check_end.py
@@ -1,7 +1,13 @@
 import os
-
+from google.cloud import storage
 for fname in os.listdir('.'):
     if fname.endswith('.mp4'):
         # do stuff on the file
-        
+        client = storage.Client()
+        # https://console.cloud.google.com/storage/browser/[bucket-id]/
+        bucket = client.get_bucket('class-hub-bucket')
+        # Then do other things...
+        blob2 = bucket.blob('remote/path/output.mp4')
+        blob2.upload_from_filename(filename='/home/class-hub1/output.mp4')
+        os.remove('output.mp4')
         break
